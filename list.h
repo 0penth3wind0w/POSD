b92bdf914536d3d3e99fc55dbbb291a1f1af25b6
0penth3wind0w <0penth3wind0w@gmail.com>
2017-10-23 17:57:55 +0800

    utList in progress ([496, X, terence_tao] = [496, Y, terence_tao])

--- list.h
+++ list.h
@@ -11,24 +11,39 @@ using std::vector;
 class List : public Term{
 public:
     List():_elements() {}
-    //List(vector<Term *> const & elements):_elements(elements){}
+    List(vector<Term *> const & elements):_elements(elements){}
+
     string symbol() const {
-        string ret = "[";
-        for(int i = 0; i < _elements.size() - 1 ; i++){
-            ret += _elements[i]-> symbol() + ", ";
+        if( _elements.empty() ){
+            string ret = "[]";
+            return ret;
         }
-        ret += _elements[_elements.size()-1]-> symbol() + "]";
-        return  ret;
+        else{
+            string ret = "[";
+            for(int i = 0; i < _elements.size() - 1 ; i++){
+                ret += _elements[i]-> symbol() + ", ";
+            }
+            ret += _elements[_elements.size()-1]-> symbol() + "]";
+            return  ret;    
+        }
     }
+
     string value() const {
-        string ret = "[";
-        for(int i = 0; i < _elements.size() - 1 ; i++){
-            ret += _elements[i]-> value() + ", ";
+        if(_elements.empty()){
+            string ret = "[]";
+            return ret;
         }
-        ret += _elements[_elements.size()-1]-> value() + "]";
-        return  ret;
+        else{
+            string ret = "[";
+            for(int i = 0; i < _elements.size() - 1 ; i++){
+                ret += _elements[i]-> value() + ", ";
+            }
+            ret += _elements[_elements.size()-1]-> value() + "]";
+            return  ret;
+        }
     }
-    bool match(Term & term);
+    
+    bool match(Term & term){ return symbol() == term.symbol(); }
     Term * head() const { return _elements.front();}
     List * tail() const ;
 
--- utList.h
+++ utList.h
@@ -3,89 +3,136 @@
 
 #include <string>
 #include <vector>
-#include "list.h"
+
 #include "term.h"
 #include "atom.h"
 #include "number.h"
 #include "variable.h"
 #include "struct.h"
+#include "list.h"
 
 using std::string;
 
 // When create a new list without any item
 // Then #symbol() of the list should return "[]"
 TEST(List, constructor) {
-    List nlist();
-    EXPECT_EQ("[]", nlist.symbol());
+    List *l = new List();
+    EXPECT_EQ("[]", l->symbol());
 }
 
 // Given there are two perfect Numbers: 8128, 496
 // When create a new list with the perfect Number
 // Then #symbol() of the list should return "[496, 8128]"
 TEST(List, Numbers) {
-    //Number num1(8128);
-    //Number num2(496);
-    //std::vector<Term *> vec = {&num1, &num2};
-    //List list(vec);
-    //EXPECT_EQ("[496,8128]", list.symbol());
+    Number num1(496);
+    Number num2(8128);
+    std::vector<Term *> vec = {&num1, &num2};
+    List numlist(vec);
+    EXPECT_EQ("[496, 8128]", numlist.symbol());
 }
-/*
+
 // Given there are two atoms: "terence_tao", "alan_mathison_turing"
 // When create a new list with the Atoms
 // Then #symbol() of the list should return "[terence_tao, alan_mathison_turing]"
 TEST(List, Atoms) {
-
+  Atom terence_tao("terence_tao");
+  Atom alan_mathison_turing("alan_mathison_turing");
+  std::vector<Term *> vec = {&terence_tao, &alan_mathison_turing};
+  List atomlist(vec);
+  EXPECT_EQ("[terence_tao, alan_mathison_turing]", atomlist.symbol());
 }
 
 // Given there are two variables: X, Y
 // When create a new list with the variables
 // Then #symbol() of the list should return "[X, Y]"
 TEST(List, Vars) {
-
+  Variable X("X");
+  Variable Y("Y");
+  std::vector<Term *> vec = {&X, &Y};
+  List varlist(vec);
+  EXPECT_EQ("[X, Y]", varlist.symbol());
 }
 
 // ?- tom = [496, X, terence_tao].
 // false.
 TEST(List, matchToAtomShouldFail) {
-
+  Atom tom("tom");
+  Number num1(496);
+  Variable X("X");
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> vec = {&num1, &X, &terence_tao};
+  List compact(vec);
+  EXPECT_FALSE(tom.match(compact));
 }
 
 // ?- 8128 = [496, X, terence_tao].
 // false.
 TEST(List, matchToNumberShouldFail) {
-
+  Number num1(8128);
+  Number num2(496);
+  Variable X("X");
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> vec = {&num2, &X, &terence_tao};
+  List compact(vec);
+  EXPECT_FALSE(num1.match(compact));
 }
 
 // ?- s(X) = [496, X, terence_tao].
 // false.
 TEST(List, matchToStructShouldFail) {
-
+  Variable X("X");
+  std::vector<Term *> vec = {&X};
+  Struct s(Atom("s"),vec);
+  Number num1(496);
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> vec1 = {&num1, &X, &terence_tao};
+  List compact(vec1);
+  EXPECT_FALSE(s.match(compact));
 }
 
 // ?- Y = [496, X, terence_tao].
 // Y = [496, X, terence_tao].
 TEST(List, matchToVarShouldSucceed) {
-
+  Variable Y("Y");
+  Number num(496);
+  Variable X("X");
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> vec = {&num, &X, &terence_tao};
+  List compact(vec);
+  EXPECT_TRUE(Y.match(compact));
 }
 
 // ?- X = [496, X, terence_tao].
 // false.
 TEST(List, matchToVarOccuredInListShouldFail) {
-
+  EXPECT_TRUE(false);
 }
 
 // ?- [496, X, terence_tao] = [496, X, terence_tao].
 // true.
 TEST(List, matchToSameListShouldSucceed) {
-
+  Number num(496);
+  Variable X("X");
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> vec = {&num, &X, &terence_tao};
+  List compact(vec);
+  EXPECT_TRUE(compact.match(compact));
 }
 
 // ?- [496, X, terence_tao] = [496, Y, terence_tao].
 // true.
 TEST(List, matchToSameListWithDiffVarNameShouldSucceed) {
-
+  Number num(496);
+  Variable X("X");
+  Variable Y("Y");
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> vec1 = {&num, &X, &terence_tao};
+  std::vector<Term *> vec2 = {&num, &Y, &terence_tao};
+  List compactX(vec1);
+  List compactY(vec2);
+  EXPECT_TRUE(compactX.match(compactY));
 }
-
+/*
 // ?- [496, X, terence_tao] = [496, 8128, terence_tao].
 // X = 8128.
 TEST(List, matchToVarToAtominListShouldSucceed) {
