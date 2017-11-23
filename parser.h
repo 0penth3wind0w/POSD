95474a277eac85308d982985ef01dd19c74da343
0penth3wind0w <0penth3wind0w@gmail.com>
2017-11-23 11:35:41 +0800

    initial commit for HW6

--- parser.h
+++ parser.h
@@ -1,89 +1,100 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-//#include <iostream>
 #include <string>
 
+#include "term.h"
 #include "atom.h"
-#include "variable.h"
 #include "number.h"
+#include "variable.h"
+#include "struct.h"
+#include "list.h"
 #include "global.h"
 #include "scanner.h"
-#include "struct.h"
 
-//using std::cout;
+
 using std::string;
+
+//#include "utParser.h"
 
 class Parser{
 public:
-  Parser(Scanner scanner) : _scanner(scanner){}
+  Parser(Scanner scanner) : _scanner(scanner), _terms(){}
+
   Term* createTerm(){
     int token = _scanner.nextToken();
-    _currentToken=token;
-    //cout<<"in\t"<<token<<"\n";
+    _currentToken = token;
     if(token == VAR){
-      //cout<<symtable[_scanner.tokenValue()].first<<"VAR \n";
       return new Variable(symtable[_scanner.tokenValue()].first);
-    }
-    else if(token == NUMBER){
-      //cout<<_scanner.tokenValue()<<" NUMBER\n";
+    }else if(token == NUMBER){
       return new Number(_scanner.tokenValue());
-    }
-    else if(token == ATOM){
-      //cout<<"atom\n";
+    }else if(token == ATOM || token == ATOMSC){
       Atom* atom = new Atom(symtable[_scanner.tokenValue()].first);
       if(_scanner.currentChar() == '(' ) {
-        _scanner.nextToken() ;
-        vector<Term*> terms = getArgs();
-        if(_currentToken == ')')
-          return new Struct(*atom, terms);
-      }
-      else
-        return atom;
-    }
-    else if(token == ATOMSC){
-      //cout<<"atomsc\n";
-      Atom* atom = new Atom(symtable[_scanner.tokenValue()].first);
-      if(_scanner.currentChar() == '(' ) {
-        _scanner.nextToken() ;
-        vector<Term*> terms = getArgs();
-        if(_currentToken == ')')
-          return new Struct(*atom, terms);
+        return structure();
       }
       else
         return atom;
     }
     else if(token == '['){
-      vector<Term*> terms = getArgs();
-      if(_currentToken == ']'){
-        List* l = new List(terms);
-        return l;
-      }
-      if(_currentToken == ')'){
-        throw std::string( "unexpected token" );
-      }
-    }      
+      return list();
+    }
+
     return nullptr;
   }
 
-  vector<Term*> getArgs()
-  {
-    Term* term = createTerm();
-    vector<Term*> args;
-    if(term)
-      args.push_back(term);
-    if((_currentToken == ']' || _currentToken == ')') && term == nullptr){
-        return args;
+
+
+  Term * structure() {
+    Atom structName = Atom(symtable[_scanner.tokenValue()].first);
+    int startIndexOfStructArgs = _terms.size();
+    _scanner.nextToken();
+    createTerms();
+    if(_currentToken == ')')
+    {
+      vector<Term *> args(_terms.begin() + startIndexOfStructArgs, _terms.end());
+      _terms.erase(_terms.begin() + startIndexOfStructArgs, _terms.end());
+      return new Struct(structName, args);
+    } else {
+      throw string("unexpected token");
     }
-    while((_currentToken = _scanner.nextToken()) == ',') {
-      args.push_back(createTerm());
-    }
-    return args;
   }
 
+  Term * list() {
+    int startIndexOfListArgs = _terms.size();
+    createTerms();
+    if(_currentToken == ']')
+    {
+      vector<Term *> args(_terms.begin() + startIndexOfListArgs, _terms.end());
+      _terms.erase(_terms.begin() + startIndexOfListArgs, _terms.end());
+      return new List(args);
+    } else {
+      throw string("unexpected token");
+    }
+  }
 
+  vector<Term *> & getTerms() {
+    return _terms;
+  }
 
 private:
+  FRIEND_TEST(ParserTest, createArgs);
+  FRIEND_TEST(ParserTest,ListOfTermsEmpty);
+  FRIEND_TEST(ParserTest,listofTermsTwoNumber);
+  FRIEND_TEST(ParserTest, createTerm_nestedStruct3);
+
+  void createTerms() {
+    Term* term = createTerm();
+    if(term!=nullptr)
+    {
+      _terms.push_back(term);
+      while((_currentToken = _scanner.nextToken()) == ',') {
+        _terms.push_back(createTerm());
+      }
+    }
+  }
+
+  vector<Term *> _terms;
   Scanner _scanner;
   int _currentToken;
 };
