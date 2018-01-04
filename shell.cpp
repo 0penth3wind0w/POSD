54e06446cfc9ec2f9cb57badced2a3ba8e0fe89b
0penth3wind0w <fc0029@yahoo.com.tw>
2018-01-04 23:19:09 +0800

    change utIterator

--- shell.cpp
+++ shell.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <string>
 
+#include "scanner.h"
 #include "parser.h"
 
 int main(){
-    
+    string input, context = "";
+    Parser *parser;
+    while (true)
+    {
+        do
+        {
+            if (context == "")
+                cout << "?- ";
+            else
+                cout << "|   ";
+            getline(cin, input);
+            if (input != "")
+                while (input[0] == ' ')
+                    input = input.substr(1, input.size() - 1);
+            context += input;
+        } while (input == "" || context.back() != '.');
+        if (context == "halt.")
+            break;
+        parser = new Parser(Scanner(context));
+        try
+        {
+            cout << parser->buildExpression()->getResult() << endl;
+        }
+        catch (string &msg)
+        {
+            cout << msg << endl;
+        }
+        context = "";
+    }
     return 0;
 }
